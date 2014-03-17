e416cc0eb52085a13eb1169effdb9ebf14f32cf6
JustH <contact@justin-hill.com>
2014-03-16 20:10:24 -0700

    add visual and data divs

--- gruntfile.js
+++ gruntfile.js
@@ -11,15 +11,23 @@ module.exports = function(grunt) {
     uglify: {
           dist: {
             files: {
-              'public/js/main.min.js' : 'javascripts/*'
+              'public/js/main.min.js' : ['javascripts/vendor/fastclick.js', 'javascripts/vendor/foundation.min.js', 'javascripts/vendor/Chart.js', 'javascripts/*.js']
             }
           }
         },
 
     watch: {
-      source: {
+      css: {
         files: ['sass/**/*.scss'],
         tasks: ['sass'],
+        options: {
+          livereload: true,
+        }
+      },
+
+      js: {
+        files: ['javascripts/*.js'],
+        tasks: ['uglify'],
         options: {
           livereload: true,
         }
@@ -28,7 +36,7 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
-
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-sass');
-  grunt.registerTask('default', ['sass']);
+  grunt.registerTask('default', ['sass', 'uglify']);
 };
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,31 @@
 $(document).foundation();
+
+// chart.js visual
+var ctx = $("#widgetChart").get(0).getContext("2d");
+
+var chartData = {
+  labels : ["January 2013","January 2014"],
+  datasets : [
+    {
+      fillColor : "rgba(46,204,113,1)",
+      strokeColor : "rgba(46,204,113,1)",
+      data : [791,573]
+    }
+  ]
+}
+
+var chartOptions = {
+  scaleFontFamily : "'Open Sans'",
+  scaleOverride : true,
+  scaleStartValue : 0,
+  scaleStepWidth : 100,
+  scaleSteps : 8,
+  scaleShowLabels : false,
+  scaleShowGridLines : false,
+  scaleFontStyle : "bold"
+  }
+
+var myNewChart = new Chart(ctx).Bar(chartData, chartOptions);
+
+
+
